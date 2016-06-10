7c9e460eae87ba594dbc40904f9b5e9ce1f781f1
Roman Volosovskyi <rasom@ukr.net>
2016-06-10 17:03:44 +0300

    call/add-listener

--- android/src/main/java/com/statusim/Jail/JailModule.java
+++ android/src/main/java/com/statusim/Jail/JailModule.java
@@ -4,6 +4,8 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
 
 public class JailModule extends ReactContextBaseJavaModule {
 
@@ -17,11 +19,20 @@ public class JailModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void parse(String js, Callback succ, Callback fail) {
+    public void parse(String chatId, String js, Callback succ, Callback fail) {
         succ.invoke("{\"commands\":{\"request-money\":" +
                 "{\"description\":\"olala!\",\"name\":\"request-money\"," +
                 "\"params\":{\"foo\":{\"type\":\"string\"}}}}," +
                 "\"responses\":{}}");
     }
-}
 
+    @ReactMethod
+    public void call(String chatId, ReadableArray path, ReadableMap params, Callback callback) {
+        callback.invoke("call");
+    }
+
+    @ReactMethod
+    public void addListener(String chatId, Callback callback) {
+        callback.invoke("addListener " + chatId);
+    }
+}
